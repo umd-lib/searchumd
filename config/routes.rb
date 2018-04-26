6f7e576d3e84f6e770fae008b9e2e3cb9ace3095
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2018-04-26 13:56:13 +0200

    LIBSEARCH-22 Add native search interface for website search
    This adds the route to the searchumd app for the 'library web site'
    searcher pages.
    
    https://issues.umd.edu/browse/LIBSEARCH-22

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 
 Rails.application.routes.draw do
   mount QuickSearch::Engine => '/'
+  mount QuickSearchLibraryWebsiteSearcher::Engine => '/search/website'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
