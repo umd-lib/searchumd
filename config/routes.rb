5c986b403577d6d419be090cdbb8480ad3016e12
David P. Steelman <dsteelma@umd.edu>
2018-08-08 11:18:07 -0400

    LIBSEARCH-61. Initial implementation of a Swiftype "native" interface
    https://issues.umd.edu/browse/LIBSEARCH-61

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount QuickSearch::Engine => '/'
   root to: 'search#index'
   get 'website' => 'website_search#index'
+  get 'swiftype' => 'swiftype_search#index'
   get 'opensearch' => 'opensearch#opensearch', :defaults => { format: 'xml' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
